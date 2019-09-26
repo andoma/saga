cdaae939eae93ea739520309cf16b5a4188f27aa
Andreas Smas <andreas@lonelycoder.com>
2019-09-26 10:03:03 -0700

    Tensor::toRGBBitmap() must synchronize() before raw access

--- src/image.cpp
+++ src/image.cpp
@@ -23,7 +23,7 @@ void Tensor::toRGBBitmap(uint8_t *output, int stride,
 
   assert(datatype == CUDNN_DATA_FLOAT);
 
-
+  synchronize();
   const float *src0 = (const float *)deviceMem();
 
   for(int y = 0; y < hh; y++) {
