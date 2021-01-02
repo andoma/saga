cc7282e8dd197ca39a301233c114daa82f3ad511
Andreas Smas <andreas@lonelycoder.com>
2021-01-02 16:40:33 +0100

    Migrate some class members from field_; to m_field; -naming

--- src/cuda/cuda_analysis.cpp
+++ src/cuda/cuda_analysis.cpp
@@ -75,32 +75,32 @@ bitset_insersects(const uint32_t *a, const uint32_t *b, int words)
 
 struct Bestfit { // Best fit allocator
 
-  size_t total_ = 0;
-  size_t peak_ = 0;
-  std::multimap<size_t, size_t> sizes_;
-  std::map<size_t, size_t> chunks_;
+  size_t m_total = 0;
+  size_t m_peak = 0;
+  std::multimap<size_t, size_t> m_sizes;
+  std::map<size_t, size_t> m_chunks;
 
   size_t alloc(size_t reqsize)
   {
     size_t offset;
-    auto it = sizes_.upper_bound(reqsize);
-    if(it == sizes_.end()) {
-      offset = total_;
-      total_ += reqsize;
-      peak_ = std::max(total_, peak_);
+    auto it = m_sizes.upper_bound(reqsize);
+    if(it == m_sizes.end()) {
+      offset = m_total;
+      m_total += reqsize;
+      m_peak = std::max(m_total, m_peak);
     } else {
 
       size_t size = it->first;
       offset = it->second;
 
-      chunks_.erase(offset);
+      m_chunks.erase(offset);
 
-      sizes_.erase(it);
+      m_sizes.erase(it);
 
       if(size > reqsize) {
         // Didn't use entire free chunk, reinsert remainder
-        sizes_.insert(std::make_pair(size - reqsize, offset + reqsize));
-        chunks_.insert(std::make_pair(offset + reqsize, size - reqsize));
+        m_sizes.insert(std::make_pair(size - reqsize, offset + reqsize));
+        m_chunks.insert(std::make_pair(offset + reqsize, size - reqsize));
       }
     }
     return offset;
@@ -109,42 +109,42 @@ struct Bestfit { // Best fit allocator
 
   void free(size_t position, size_t size)
   {
-    auto next = chunks_.find(position + size);
-    if(next != chunks_.end()) {
+    auto next = m_chunks.find(position + size);
+    if(next != m_chunks.end()) {
       // Merge next block
       eraseInSize(next->second, next->first);
       size += next->second;
-      chunks_.erase(next);
+      m_chunks.erase(next);
     }
 
-    auto prev = chunks_.lower_bound(position);
-    if(prev != chunks_.begin()) {
+    auto prev = m_chunks.lower_bound(position);
+    if(prev != m_chunks.begin()) {
       // Merge prev block
       prev--;
       if(prev->first + prev->second == position) {
         eraseInSize(prev->second, prev->first);
         size += prev->second;
         position = prev->first;
-        chunks_.erase(prev);
+        m_chunks.erase(prev);
       }
     }
 
-    if(size + position == total_) {
-      total_ = position;
+    if(size + position == m_total) {
+      m_total = position;
     } else {
-      sizes_.insert(std::make_pair(size, position));
-      chunks_.insert(std::make_pair(position, size));
+      m_sizes.insert(std::make_pair(size, position));
+      m_chunks.insert(std::make_pair(position, size));
     }
   }
 
 
   void eraseInSize(size_t size, size_t position)
   {
-    auto range = sizes_.equal_range(size);
+    auto range = m_sizes.equal_range(size);
     int found = 0;
     for(auto it = range.first; it != range.second;) {
       if(it->second == position) {
-        it = sizes_.erase(it);
+        it = m_sizes.erase(it);
         found++;
       } else {
         it++;
@@ -156,14 +156,14 @@ struct Bestfit { // Best fit allocator
   void dump()
   {
     printf("Bestfit heap peak:%zd current:%zd\n",
-           peak_, total_);
+           m_peak, m_total);
     printf("chunks\n");
-    for(auto it : chunks_) {
+    for(auto it : m_chunks) {
       printf("\t%zd + %zd\n", it.first, it.second);
     }
 
     printf("sizes\n");
-    for(auto it : sizes_) {
+    for(auto it : m_sizes) {
       printf("\t%zd @ %zd\n", it.first, it.second);
     }
   }
@@ -180,47 +180,47 @@ struct Liveness {
 
   Liveness(size_t words)
   {
-    out_ = (uint32_t *)calloc(words, sizeof(uint32_t));
-    in_  = (uint32_t *)calloc(words, sizeof(uint32_t));
-    gen_ = (uint32_t *)calloc(words, sizeof(uint32_t));
-    def_ = (uint32_t *)calloc(words, sizeof(uint32_t));
+    m_out = (uint32_t *)calloc(words, sizeof(uint32_t));
+    m_in  = (uint32_t *)calloc(words, sizeof(uint32_t));
+    m_gen = (uint32_t *)calloc(words, sizeof(uint32_t));
+    m_def = (uint32_t *)calloc(words, sizeof(uint32_t));
   }
 
   ~Liveness()
   {
-    free(out_);
-    free(in_);
-    free(gen_);
-    free(def_);
+    free(m_out);
+    free(m_in);
+    free(m_gen);
+    free(m_def);
   }
 
-  uint32_t *out_;
-  uint32_t *in_;
+  uint32_t *m_out;
+  uint32_t *m_in;
 
   // Set of varibles used before any assignment (input tensors)
-  uint32_t *gen_;
+  uint32_t *m_gen;
 
   // Set of variables written to (output tensors)
-  uint32_t *def_;
+  uint32_t *m_def;
 };
 
 
 struct LiveAnalysis {
 
-  std::vector<std::shared_ptr<CudaOperation>> ops_;
+  std::vector<std::shared_ptr<CudaOperation>> m_ops;
 
-  int ln_id_base_;
-  int ln_words_;
-  int ln_size_;
-  std::vector<std::shared_ptr<Liveness>> lns_;
-  std::vector<size_t> memory_usage_;
-  std::vector<std::shared_ptr<CudaTensorStorage>> storage_;
+  int m_ln_id_base;
+  int m_ln_words;
+  int m_ln_size;
+  std::vector<std::shared_ptr<Liveness>> m_lns;
+  std::vector<size_t> m_memory_usage;
+  std::vector<std::shared_ptr<CudaTensorStorage>> m_storage;
 
-  uint32_t *exported_gen_;
+  uint32_t *m_exported_gen;
 
   LiveAnalysis(const std::vector<std::shared_ptr<CudaOperation>> &ops,
                const std::vector<std::shared_ptr<CudaTensorStorage>> &exported)
-    : ops_(ops)
+    : m_ops(ops)
   {
     int lowest_id = INT32_MAX;
     int highest_id = INT32_MIN;
@@ -245,60 +245,60 @@ struct LiveAnalysis {
       highest_id = std::max(highest_id, id);
     }
 
-    ln_id_base_ = lowest_id;
-    ln_size_ = highest_id - lowest_id + 1;
-    ln_words_ = (ln_size_ + 31) / 32;
+    m_ln_id_base = lowest_id;
+    m_ln_size = highest_id - lowest_id + 1;
+    m_ln_words = (m_ln_size + 31) / 32;
 
-    lns_.reserve(ops.size());
+    m_lns.reserve(ops.size());
 
-    memory_usage_.resize(ln_size_);
-    storage_.resize(ln_size_);
+    m_memory_usage.resize(m_ln_size);
+    m_storage.resize(m_ln_size);
 
-    exported_gen_ = (uint32_t *)calloc(ln_words_, sizeof(uint32_t));
+    m_exported_gen = (uint32_t *)calloc(m_ln_words, sizeof(uint32_t));
 
     for(const auto &s : exported) {
-      const int id = s->m_id - ln_id_base_;
-      bitset(exported_gen_, id);
+      const int id = s->m_id - m_ln_id_base;
+      bitset(m_exported_gen, id);
     }
 
     for(const auto &op : ops) {
-      auto ln = std::make_shared<Liveness>(ln_words_);
+      auto ln = std::make_shared<Liveness>(m_ln_words);
 
       for(const auto &t : op->getInputs()) {
-        const int id = t->storage_id() - ln_id_base_;
-        bitset(ln->gen_, id);
-        memory_usage_[id] = t->memoryUsage();
-        storage_[id] = t->m_storage;
+        const int id = t->storage_id() - m_ln_id_base;
+        bitset(ln->m_gen, id);
+        m_memory_usage[id] = t->memoryUsage();
+        m_storage[id] = t->m_storage;
       }
 
       for(const auto &t : op->getOutputs()) {
-        const int id = t->storage_id() - ln_id_base_;
-        bitset(ln->def_, id);
-        memory_usage_[id] = t->memoryUsage();
-        storage_[id] = t->m_storage;
+        const int id = t->storage_id() - m_ln_id_base;
+        bitset(ln->m_def, id);
+        m_memory_usage[id] = t->memoryUsage();
+        m_storage[id] = t->m_storage;
 
         // This is a bit of a hack.
         // If writing to an offset in the tensor's storage we assume
         // it's the non-first part of a concat (or similar operation)
         if(t->m_offset)
-          bitset(ln->gen_, id);
+          bitset(ln->m_gen, id);
 
       }
-      lns_.push_back(ln);
+      m_lns.push_back(ln);
     }
     update();
   }
 
   ~LiveAnalysis()
   {
-    free(exported_gen_);
+    free(m_exported_gen);
   }
 
   void update() {
-    uint32_t in_prim[ln_words_];
-    uint32_t out_buf[ln_words_];
+    uint32_t in_prim[m_ln_words];
+    uint32_t out_buf[m_ln_words];
 
-    if(ops_.size() == 0)
+    if(m_ops.size() == 0)
       return;
 
     int stable = 0;
@@ -313,26 +313,26 @@ struct LiveAnalysis {
 
       const uint32_t *out_prim;
 
-      Liveness *succ = lns_[0].get();
-      memcpy(out_buf, succ->in_, ln_words_ * sizeof(uint32_t));
-      bitset_or(out_buf, exported_gen_, ln_words_);
+      Liveness *succ = m_lns[0].get();
+      memcpy(out_buf, succ->m_in, m_ln_words * sizeof(uint32_t));
+      bitset_or(out_buf, m_exported_gen, m_ln_words);
       out_prim = out_buf;
 
-      for(ssize_t i = ops_.size() - 1; i >= 0; i--) {
-        Liveness *cur  = lns_[i].get();
+      for(ssize_t i = m_ops.size() - 1; i >= 0; i--) {
+        Liveness *cur  = m_lns[i].get();
 
-        for(int j = 0; j < ln_words_; j++)
-          in_prim[j] = (out_prim[j] & ~cur->def_[j]) | cur->gen_[j];
+        for(int j = 0; j < m_ln_words; j++)
+          in_prim[j] = (out_prim[j] & ~cur->m_def[j]) | cur->m_gen[j];
 
-        if(memcmp(cur->out_, out_prim, ln_words_ * sizeof(uint32_t)) ||
-           memcmp(cur->in_,  in_prim,  ln_words_ * sizeof(uint32_t))) {
+        if(memcmp(cur->m_out, out_prim, m_ln_words * sizeof(uint32_t)) ||
+           memcmp(cur->m_in,  in_prim,  m_ln_words * sizeof(uint32_t))) {
 
           stable = 0;
-          memcpy(cur->out_, out_prim, ln_words_ * sizeof(uint32_t));
-          memcpy(cur->in_,  in_prim,  ln_words_ * sizeof(uint32_t));
+          memcpy(cur->m_out, out_prim, m_ln_words * sizeof(uint32_t));
+          memcpy(cur->m_in,  in_prim,  m_ln_words * sizeof(uint32_t));
         }
 
-        out_prim = cur->in_;
+        out_prim = cur->m_in;
       }
     }
   }
@@ -342,31 +342,31 @@ struct LiveAnalysis {
 
     while(1) {
       bool did_something = false;
-      for(ssize_t i = ops_.size() - 1; i >= 0; i--) {
+      for(ssize_t i = m_ops.size() - 1; i >= 0; i--) {
         bool kill = true;
-        for(int j = 0; j < ln_words_; j++) {
-          if(lns_[i]->out_[j] & lns_[i]->def_[j]) {
+        for(int j = 0; j < m_ln_words; j++) {
+          if(m_lns[i]->m_out[j] & m_lns[i]->m_def[j]) {
             kill = false;
             break;
           }
         }
         if(kill) {
-          ops_.erase(ops_.begin() + i);
-          lns_.erase(lns_.begin() + i);
+          m_ops.erase(m_ops.begin() + i);
+          m_lns.erase(m_lns.begin() + i);
           did_something = true;
         } else {
-          for(int j = 0; j < ln_words_; j++) {
-            if((lns_[i]->out_[j] & lns_[i]->def_[j]) != lns_[i]->def_[j]) {
-              uint32_t out = lns_[i]->out_[j];
-              uint32_t def = lns_[i]->def_[j];
+          for(int j = 0; j < m_ln_words; j++) {
+            if((m_lns[i]->m_out[j] & m_lns[i]->m_def[j]) != m_lns[i]->m_def[j]) {
+              uint32_t out = m_lns[i]->m_out[j];
+              uint32_t def = m_lns[i]->m_def[j];
               for(int k = 0; k < 32; k++) {
                 if(((1 << k) & def) && !((1 << k) & out)) {
                   int id = j * 32 + k;
-                  auto s = storage_[id];
-                  if(!ops_[i]->killOutput(s)) {
+                  auto s = m_storage[id];
+                  if(!m_ops[i]->killOutput(s)) {
                     fprintf(stderr, "Unable to kill dead output T%d\n",
-                            id + ln_id_base_);
-                    ops_[i]->print(true);
+                            id + m_ln_id_base);
+                    m_ops[i]->print(true);
                     exit(1);
                   }
                 }
@@ -385,23 +385,23 @@ struct LiveAnalysis {
 
   void print(bool liveranges = false) {
 
-    for(size_t i = 0; i <  ops_.size(); i++) {
+    for(size_t i = 0; i <  m_ops.size(); i++) {
       if(liveranges && i == 0) {
         printf("Live:");
-        for(int j = 0; j < ln_size_; j++) {
-          if(bitchk(lns_[i]->in_, j)) {
-            printf(" T%d", j + ln_id_base_);
+        for(int j = 0; j < m_ln_size; j++) {
+          if(bitchk(m_lns[i]->m_in, j)) {
+            printf(" T%d", j + m_ln_id_base);
           }
         }
         printf("\n");
       }
 
-      printf("%4zd: %s\n", i, ops_[i]->str().c_str());
+      printf("%4zd: %s\n", i, m_ops[i]->str().c_str());
       if(liveranges) {
         printf(" Live:");
-        for(int j = 0; j < ln_size_; j++) {
-          if(bitchk(lns_[i]->out_, j)) {
-            printf(" T%d", j + ln_id_base_);
+        for(int j = 0; j < m_ln_size; j++) {
+          if(bitchk(m_lns[i]->m_out, j)) {
+            printf(" T%d", j + m_ln_id_base);
           }
         }
         printf("\n");
@@ -413,12 +413,12 @@ struct LiveAnalysis {
 
     size_t peak = 0;
 
-    for(size_t i = 0; i <  ops_.size(); i++) {
+    for(size_t i = 0; i <  m_ops.size(); i++) {
 
       size_t s = 0;
-      for(int j = 0; j < ln_size_; j++) {
-        if(bitchk(lns_[i]->in_, j) || bitchk(lns_[i]->out_, j)) {
-          s += memory_usage_[j];
+      for(int j = 0; j < m_ln_size; j++) {
+        if(bitchk(m_lns[i]->m_in, j) || bitchk(m_lns[i]->m_out, j)) {
+          s += m_memory_usage[j];
         }
       }
       if(s > peak) {
@@ -433,17 +433,17 @@ struct LiveAnalysis {
     if(from == to)
       return;
 
-    auto op = ops_[from];
-    auto ln = lns_[from];
+    auto op = m_ops[from];
+    auto ln = m_lns[from];
 
-    ops_.erase(ops_.begin() + from);
-    lns_.erase(lns_.begin() + from);
+    m_ops.erase(m_ops.begin() + from);
+    m_lns.erase(m_lns.begin() + from);
 
     if(from < to) {
       to--;
     }
-    ops_.insert(ops_.begin() + to, op);
-    lns_.insert(lns_.begin() + to, ln);
+    m_ops.insert(m_ops.begin() + to, op);
+    m_lns.insert(m_lns.begin() + to, ln);
   }
 
   // Try to move operations further up in program order if
@@ -452,11 +452,11 @@ struct LiveAnalysis {
   {
     size_t memory_use = peakMemoryUsage();
 
-    for(ssize_t i = 0; i < (ssize_t)ops_.size(); i++) {
-      const uint32_t *gen = lns_[i]->gen_;
+    for(ssize_t i = 0; i < (ssize_t)m_ops.size(); i++) {
+      const uint32_t *gen = m_lns[i]->m_gen;
       ssize_t j = -1;
       for(j = i - 1; j >= 0; j--) {
-        if(bitset_insersects(gen, lns_[j]->def_, ln_words_))
+        if(bitset_insersects(gen, m_lns[j]->m_def, m_ln_words))
           break;
       }
 
@@ -487,10 +487,10 @@ struct LiveAnalysis {
     std::unordered_map<int, size_t> positions;
     std::unordered_set<int> inuse;
 
-    for(size_t i = 0; i <  ops_.size(); i++) {
-      for(int j = 0; j < ln_size_; j++) {
-        if(bitchk(lns_[i]->out_, j) && !bitchk(lns_[i]->in_, j)) {
-          const size_t size = (memory_usage_[j] + align - 1) / align;
+    for(size_t i = 0; i <  m_ops.size(); i++) {
+      for(int j = 0; j < m_ln_size; j++) {
+        if(bitchk(m_lns[i]->m_out, j) && !bitchk(m_lns[i]->m_in, j)) {
+          const size_t size = (m_memory_usage[j] + align - 1) / align;
 
           assert(inuse.find(j) == inuse.end());
           inuse.insert(j);
@@ -498,9 +498,9 @@ struct LiveAnalysis {
         }
       }
 
-      for(int j = 0; j < ln_size_; j++) {
-        if(bitchk(lns_[i]->in_, j) && !bitchk(lns_[i]->out_, j)) {
-          const size_t size = (memory_usage_[j] + align - 1) / align;
+      for(int j = 0; j < m_ln_size; j++) {
+        if(bitchk(m_lns[i]->m_in, j) && !bitchk(m_lns[i]->m_out, j)) {
+          const size_t size = (m_memory_usage[j] + align - 1) / align;
 
           assert(inuse.find(j) != inuse.end());
           inuse.erase(j);
@@ -513,9 +513,9 @@ struct LiveAnalysis {
 
     r->table_.reserve(positions.size());
     for(const auto &p : positions) {
-      r->table_.push_back(std::make_pair(storage_[p.first], p.second));
+      r->table_.push_back(std::make_pair(m_storage[p.first], p.second));
     }
-    r->size_ = bf.peak_ * align;
+    r->size_ = bf.m_peak * align;
     return r;
   }
 };
@@ -528,7 +528,7 @@ reduceLiveranges(const std::vector<std::shared_ptr<CudaOperation>> &ops,
   LiveAnalysis la(ops, exported);
   la.eliminateDeadInstructions();
   la.reduceMemoryPressure();
-  return la.ops_;
+  return la.m_ops;
 }
 
 
