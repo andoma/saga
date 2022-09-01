7114f9123e942c16daf8b347e05ce743813da065
Andreas Smas <andreas@lonelycoder.com>
2022-09-01 08:30:08 +0200

    Expand on the fc-test tool

--- test/test_fc.cpp
+++ test/test_fc.cpp
@@ -13,15 +13,28 @@ using namespace saga;
 static int
 fc_main(int argc, char **argv)
 {
-    int batch_size = 2;
     int opt;
+
+    int inputs = 4;
+    int outputs = 8;
+    int batch_size = 2;
+    bool transposed = false;
 
     srand(getpid() ^ time(NULL));
 
     auto dt = Tensor::DataType::FLOAT;
 
-    while((opt = getopt(argc, argv, "b:h")) != -1) {
+    while((opt = getopt(argc, argv, "i:o:tb:h")) != -1) {
         switch(opt) {
+        case 'i':
+            inputs = atoi(optarg);
+            break;
+        case 'o':
+            outputs = atoi(optarg);
+            break;
+        case 't':
+            transposed = true;
+            break;
         case 'b':
             batch_size = atoi(optarg);
             break;
@@ -35,47 +48,90 @@ fc_main(int argc, char **argv)
 
     Graph g;
 
-    auto input = makeTensor(dt, Dims({DimParam::BATCH_SIZE, 4}), "input");
-    auto n = g.addNode("fc", input, {{"outputs", 8}}, "fc");
-    auto output1 = n->y();
-    n = g.addNode("fc", n, {{"outputs", 4}, {"transW", true}}, "fc");
+    auto input = makeTensor(dt, Dims({DimParam::BATCH_SIZE, inputs}), "input");
 
-    auto output2 = n->y();
+    std::shared_ptr<Tensor> weights;
+
+    if(transposed) {
+        weights = makeTensor(dt, Dims({outputs, inputs}), "weights");
+    } else {
+        weights = makeTensor(dt, Dims({inputs, outputs}), "weights");
+    }
+
+    auto target =
+        makeTensor(dt, Dims({DimParam::BATCH_SIZE, outputs}), "target");
+
+    auto n = g.addNode("fc", {{"x", input}, {"w", weights}},
+                       {{"outputs", outputs}, {"transW", transposed}}, "fc");
+
+    auto output = n->y();
+
+    auto l = g.addNode("loss", {{"x", n->y()}, {"target", target}});
+    auto result = l->y();
+
     auto p = ctx->createProgram({g, .batch_size = batch_size},
-                                ProgramType::INFERENCE, {});
+                                ProgramType::TRAINING, {.learning_rate = 0.1});
 
+    auto activations = ctx->resolveTensor(input->grad());
     input = ctx->resolveTensor(input);
-    auto weights = ctx->resolveTensor(n->inputs_["w"]);
-    output1 = ctx->resolveTensor(output1);
-    output2 = ctx->resolveTensor(output2);
+    weights = ctx->resolveTensor(weights);
+    result = ctx->resolveTensor(result);
+    target = ctx->resolveTensor(target);
+    auto grad = ctx->resolveTensor(output->grad());
+    output = ctx->resolveTensor(output);
+    auto mmss = ctx->resolveTensor(l->outputs_["mmss"]);
 
     auto input_ta = input->access();
-    for(size_t i = 0; i < 4; i++) {
-        input_ta->set(Dims{{0, (int)i}}, -0.75f + i * 0.5f);
-        //        input_ta->set(Dims{{1, (int)i}}, i * 10);
+    for(int n = 0; n < batch_size; n++) {
+        for(int i = 0; i < inputs; i++) {
+            input_ta->set(Dims{{n, i}}, 1 + n * 10 + i);
+        }
     }
-#if 0
+
     auto weights_ta = weights->access();
-    for(size_t i = 0; i < 4; i++) {
-        weights_ta->set(Dims{{(int)i, 0}}, 1.0f - i * 0.1f);
-        weights_ta->set(Dims{{(int)i, 1}}, i * 0.1f);
+    for(int i = 0; i < inputs; i++) {
+        if(transposed) {
+            weights_ta->set(Dims{{0, i}}, i);
+        } else {
+            weights_ta->set(Dims{{i, 0}}, i);
+        }
     }
+
+    auto target_ta = target->access();
+    for(int n = 0; n < batch_size; n++) {
+#if 0
+        for(int i = 0; i < outputs; i++) {
+            target_ta->set(Dims{{n, i}}, 1 + (10 - n) * 10 + (10 - i));
+        }
 #endif
+        int i = 1;
+        target_ta->set(Dims{{n, i}}, n + 1);
+    }
+
     p->finalize();
+
+    p->dump(stdout, true);
+
+    weights->print("WEIGHTS");
+    printf("\n");
 
     p->run();
 
-    p->dump(stdout, true);
     printf("\n");
     input->print("  INPUT");
     printf("\n");
     weights->print("WEIGHTS");
     printf("\n");
-    output1->print("OUTPUT1");
+    result->print("RESULT");
     printf("\n");
-    output2->print("OUTPUT2");
+    target->print("TARGET");
     printf("\n");
-    output2->printStats("OUTPUT2");
+    grad->print("GRAD");
+    printf("\n");
+    activations->print("ACTIVATIONS");
+    printf("\n");
+    //    mmss->print("MMSS");
+    //    printf("\n");
     return 0;
 }
 
